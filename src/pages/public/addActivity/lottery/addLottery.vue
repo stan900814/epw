ac1773629c11cc1b9f5833406e85cab193ae197e
stan900814 <362328861@qq.com>
2020-03-29 19:15:55 +0800

    优惠卷，抽奖

--- src/pages/public/addActivity/lottery/addLottery.vue
+++ src/pages/public/addActivity/lottery/addLottery.vue
@@ -1,19 +1,95 @@
 <template>
-	<view></view>
+	<view>
+		<view>
+			<form>
+				<view>
+					<text>活动名称：</text>
+					<input type="text" placeholder="请输入此次抽奖活动名称" v-model="lotteryInfo.name">
+				</view>
+				<view>
+					<text>上传图片</text>
+					<img src="*" alt="图片占位" @tap="picImage">
+					<view v-for="(item,index) in lotteryImgList" :key="index">
+						<img src="lotteryImgList[index]">
+					</view>
+				</view>
+				<view>
+					<text>奖品：</text>
+					<input type="text" placeholder="例:iphoneX一台" v-model="lotteryInfo.prize">
+				</view>
+				<view>
+					<text>详细信息：</text>
+					<input type="text" placeholder="" v-model="lotteryInfo.detail">折
+				</view>
+				<view>
+					<text>抽奖人数下限：</text>
+					<input type="text" placeholder="请输入最少人数" v-model="lotteryInfo.minAmount">人
+				</view>
+				<view>
+					<text>抽奖人数上限：</text>
+					<input type="text" placeholder="请输入最少人数" v-model="lotteryInfo.maxAmount">人
+				</view>
+				<view>
+					<text>中奖人数：</text>
+					<input type="text" placeholder="请输入中奖人数" v-model="lotteryInfo.awardAmount">人
+				</view>
+				<view>
+					<text>开始时间</text>
+					<picker mode="time" v-model="lotteryInfo.start">请选择时间 ></picker>
+				</view>
+				<view>
+					<text>活动持续时间</text>
+					<input type="text" placeholder="" v-model="lotteryInfo.duration">天
+				</view>
+				<view>
+					<text>开奖时间</text>
+					<picker mode="time" v-model="lotteryInfo.end">请选择时间 ></picker>
+				</view>
+				<button @tap="submitHandle">提交</button>
+			</form>
+		</view>
+	</view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-
+				lotteryImgList:Array,
+				lotteryInfo:{
+					"name":String,
+					"prize":String,
+					"detail":String,
+					"minAmount":String,
+					"maxAmount":String,
+					"awardAmount":String,
+					"start":String,
+					"end":String,
+					"duration":String,
+				}	
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			picImage(){
+				uni.chooseImage({
+					sourceType:['camera','album'],
+					success:()=>{
+						this.lotteryImgList = res.tempFilePaths
+					}
+				})
+			},
+			submitHandle(){
+				console.log('----------')
+				console.log(lotteryImgList)
+				console.log('----------')
+				console.log(lotteryInfo)
+				uni.navigateTo({
+					url:'./lotteryInfo.vue'
+				})
+			}
 		}
 	}
 </script>
--- src/pages/public/addActivity/volume/addVolume.vue
+++ src/pages/public/addActivity/volume/addVolume.vue
@@ -1,19 +1,82 @@
 <template>
-	<view></view>
+	<view>
+		<view>
+			<form>
+				<view>
+					<text>名称：</text>
+					<input type="text" placeholder="请输入" v-model="volumeInfo.name">
+				</view>
+				<view>
+					<text>选择优惠卷封面：</text>
+					<img src="*" alt="图片占位" @tap="picImage">
+					<view v-for="(item,index) in volumeImgList" :key="index">
+						<img src="volumeImgList[index]">
+					</view>
+				</view>
+				<view>
+					<text>优惠类型：</text>
+					<picker disabled>通用</picker>
+				</view>
+				<view>
+					<text>折扣力度：</text>
+					<input type="text" placeholder="例：9.5" v-model="volumeInfo.discount">折
+				</view>
+				<view>
+					<text>数量：</text>
+					<input type="text" placeholder="例：10" v-model="volumeInfo.amount">
+				</view>
+				<view>
+					<text>适用商品：{{goods.name}}</text>
+					
+				</view>
+				<view>
+					<input type="text" placeholder="备注" v-model="volumeInfo.marks">
+				</view>
+				<button @tap="submitHandle">提交</button>
+			</form>
+		</view>
+	</view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				title: ''
+				volumeImgList:Array,volume,
+				goods:{
+					"name":String,
+				},
+				volumeInfo:{
+					"name":String,
+					"marks":String,
+					"amount":String,
+					"discount":String
+				}	
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			picImage(){
+				uni.chooseImage({
+					sourceType:['camera','album'],
+					success:()=>{
+						this.volumeImgList = res.tempFilePaths
+					}
+				})
+			},
+			submitHandle(){
+				console.log('----------')
+				console.log(volumeImgList)
+				console.log('----------')
+				console.log(goods)
+				console.log('----------')
+				console.log(volumeInfo)
+				uni.navigateTo({
+					url:'./volumeInfo.vue'
+				})
+			}
 		}
 	}
 </script>
