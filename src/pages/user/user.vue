b8dd57ab0dc6e0f301299a99e662f06b627f718f
stan900814 <362328861@qq.com>
2020-03-30 15:01:44 +0800

    引入uniui

--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -1,13 +1,48 @@
 <template>
-	<view></view>
+	<view>
+		<view>
+			管理
+			<uni-grid :column="3" :show-border="false"  :square="false">
+				<uni-grid-item>
+					<text class="text">店铺管理</text>
+				</uni-grid-item>
+				<uni-grid-item>
+					<text class="text">购买服务</text>
+				</uni-grid-item>
+			</uni-grid>
+		</view>
+		<view>
+			设置
+			<uni-grid :column="3" :show-border="false"  :square="false">
+				<uni-grid-item>
+					<text class="text">门店搜索</text>
+				</uni-grid-item>
+				<uni-grid-item>
+					<text class="text">门店入驻</text>
+				</uni-grid-item>
+				<uni-grid-item>
+					<text class="text">门店选择</text>
+				</uni-grid-item>
+				<uni-grid-item>
+					<text class="text">人员审核</text>
+				</uni-grid-item>
+			</uni-grid>
+		</view>
+	</view>
 </template>
 
 <script>
+import uniGrid from "@dcloudio/uni-ui/lib/uni-grid/uni-grid"
+import uniGridItem from "@dcloudio/uni-ui/lib/uni-grid-item/uni-grid-item"
 	export default {
 		data() {
 			return {
 				title: ''
 			}
+		},
+		components:{
+			uniGrid,
+			uniGridItem
 		},
 		onLoad() {
 
